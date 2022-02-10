36ab3008588278beacbf52f9c734ebe6bd45cbcb
billyberkouwer <billyberkouwer@live.com>
2022-02-10 21:42:10 +0000

    netlify

--- src/World/systems/changePosition.js
+++ src/World/systems/changePosition.js
@@ -3,8 +3,7 @@ function createChangePosition(button, controls, camera, car) {
         controls.enabled = false;
         camera.tick = (delta) => {
             camera.lookAt(car.position)
-            camera.position.y -= Math.cos(3.14) * delta;
-            camera.position.z += Math.cos(3.14) * 5 * delta;
+            camera.position.z +=  5 * delta;
         };
         console.log('moving')
         setTimeout(function() {
